365735c40296273b77963f460e9d4f811ac92a6c
Julio Cadelca <contatojp30@gmail.com>
2024-01-27 16:59:31 -0300

    Update MaioreMenorValoresLista.py

--- ex.py/MaioreMenorValoresLista.py
+++ ex.py/MaioreMenorValoresLista.py
@@ -1,5 +1,5 @@
 # Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem crescente.
-
+#
 valores = list()
 menor = 0
 maior = 0
